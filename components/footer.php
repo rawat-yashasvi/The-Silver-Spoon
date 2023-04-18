70f6dfd663882991446d066ac8971a36413b578b
rawat-yashasvi <121218747+rawat-yashasvi@users.noreply.github.com>
2023-04-18 14:45:31 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>Our email</h3>
+         <a href="mailto:">21CS3055@rgipt.ac.in</a>
+         
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>Opening Hours</h3>
+         <p>10:00am to 11:30pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>Our address</h3>
+         <a href="#">Lucknow, India </a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:8824434970">8824434970</a>
+         
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Web Technology Project</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
